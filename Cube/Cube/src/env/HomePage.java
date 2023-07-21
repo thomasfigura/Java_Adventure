b8bca5208c5c544d9427dc7879afafdf9afd84fe
tjf87 <tjf87@cornell.edu>
2023-07-21 00:11:32 -0700

    Added a Simple UI

---
+++ Cube/Cube/src/env/HomePage.java
@@ -0,0 +1,46 @@
+package env;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class HomePage extends JFrame {
+    JFrame home;
+    public HomePage(int width, int height) {
+        home = new JFrame("Home");
+        home.setSize(width, height);
+        home.setVisible(true);
+        home.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JButton button = new JButton("Start");
+
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    startDrawing();
+                } catch (Exception t) {
+                }
+            }
+        });
+
+        home.add(button);
+
+    }
+
+    public void startDrawing() throws InterruptedException {
+        Enviorment envi = new Enviorment();
+        JFrame application = new JFrame("Wall");
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        application.add(envi);
+        application.setSize(400, 400);
+        application.setVisible(true);
+        home.add(application);
+        while (true) {
+            envi.repaint();
+        }
+    }
+}
--- Cube/Cube/src/env/main.java
+++ Cube/Cube/src/env/main.java
@@ -7,16 +7,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class main {
-    public static void main(String[] args) throws InterruptedException{
-        Enviorment envi = new Enviorment();
-        JFrame application = new JFrame("Wall");
-        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        application.add(envi);
-        application.setSize(400,400);
-        application.setVisible(true);
-        while(true){
-            Thread.sleep(100);
-            envi.repaint();
-        }
-    }  
+    public static void main(String[] args) throws InterruptedException {
+        HomePage h = new HomePage(400,400);
+    }
 }
